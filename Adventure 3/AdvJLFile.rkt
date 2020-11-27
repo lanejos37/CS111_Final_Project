503dbbb091334a9f804880bfc862731337f4117e
lanejos37 <74506238+lanejos37@users.noreply.github.com>
2020-11-27 17:42:18 -0600

    Update AdvJLFile.rkt

--- Adventure 3/AdvJLFile.rkt
+++ Adventure 3/AdvJLFile.rkt
@@ -2,6 +2,9 @@
 (require "macros.rkt")
 (require "utilities.rkt")
 
+;;Code to check if a string is in a list
+(define (ismember1? str strs) (ormap [lambda (s) (string=? s str)] strs))
+
 ;; Initial message display at the start of the game
 (display "You are playing minecraft and are going through caves. You are currently in your underground room and are going to explore the cave system you just found next to your place.\nUse the actions procedure to see what actions are possible.")
 
@@ -13,17 +16,19 @@
 
 ;;Defining the general struct object with a method that describes the object. Now all objects are objects they are just something that need a description.
 (define-struct object
-  (adjectives)
+  (adjectives name)
   #:methods
   (define (description a)
-    (object-adjectives a))
+    (if (ismember1? (object-name a) (room-viewroom currentroom))
+    (object-adjectives a)
+    (display "Cannot observe object that is not in your room")))
   (define(objectlist a)
     (type-name-string a)))
 
 
 ;;New "object" called room
 (define-struct (room object)
-  (viewroom name))
+  (viewroom))
 
 ;;allows you to view what is in the room/cave
 (define (viewroom)
@@ -31,25 +36,23 @@
 
 ;;room you start in
 (define home
-  (make-room "your own underground home where your adventure begins!" (list "chest" "cave1") "home"))
+  (make-room "your own underground home where your adventure begins!" "home" (list "chest" "cave1")))
 
 ;;cave you can enter
 (define cave1
-  (make-room "first cave in the cave system you have entered" (list "creeper1" "netherportal") "cave1"))
+  (make-room "first cave in the cave system you have entered" "cave1" (list "creeper1" "netherportal")))
 
 ;;netherportal takes you take nether
 (define netherportal
-  (make-room "netherportal takes you to the nether" (list "zombie_pigman" "") "netherportal"))
+  (make-room "netherportal takes you to the nether" "netherportal" (list "zombie_pigman" "")))
 
 ;;initializes the value for current room
 (define currentroom
   home)
 
 ;;updates current room when you enter a new room
-(define (ismember1? str strs) (ormap [lambda (s) (string=? s str)] strs))
-
 (define (enter newroom)
-  (if (ismember1? (room-name newroom) (room-viewroom currentroom))
+  (if (ismember1? (object-name newroom) (room-viewroom currentroom))
   (set! currentroom newroom)
   (display "Cannot enter a non-adjacent room")))
 
@@ -63,11 +66,11 @@
  
 ;;One time of container that can always be accessed
 (define player-inventory
-  (make-container "personal inventory with max storage of 10 objects" (list "map")))
+  (make-container "player-inventory" "personal inventory with max storage of 10 objects" (list "map")))
 
 ;;New container that should only be accesible when in the room it is in
 (define chest
-  (make-container "chest with max storage of 10 different objects" (list "axe" "apple" "2 diamonds")))
+  (make-container "chest" "chest with max storage of 10 different objects" (list "axe" "apple" "2 diamonds")))
 
 ;;New struct creeper that is designed to make the player die if thet approach the creeper
 (define-struct (mobs object)
@@ -80,7 +83,8 @@
 (define (attack_pigman a)
   (if (> (2) 0.5)
       (error "you attracted the hoard, and died")
-      (begin (destroy zombie_pigman) (display "Slayed the zombie pigmen horde")))))
+      void)))
+      ;(destroy  
      
 
  
@@ -93,7 +97,7 @@
 
 ;;creeper in cave1 
 (define creeper1
-  (make-creeper "makes a hissing noice when you come close"))
+  (make-creeper "creeper1" "makes a hissing noice when you come close"))
 
 ;;Notes: Need to make it so that objects and containers are not accesible when in different rooms.
 ;;Also need to get rid of creeper when it exploded.
